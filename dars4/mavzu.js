876905a0c149083a8ef660387e7aff8b11640ca2
IlyosbekKazimjanov <ilyosbek.351.60.96@gmail.com>
2025-05-19 20:50:19 +0500

    dars

---
+++ dars4/mavzu.js
@@ -0,0 +1,102 @@
+// let sintac = 1;
+// if (sintac >= 0) console.log("hi");
+// else console.log("bye");
+
+// let tem = 10;
+// if (tem > 25) {
+//   console.log("havo issiq");
+// } else {
+//   console.log("havo sovuq");
+// }
+// let bal = 88;
+// if (bal >= 80) {
+//   console.log("tabriklayman siz imtihondan otingiz ");
+// } else {
+//   ("afsuski imtihondan ota olmadingiz");
+// }
+// let number = -5;
+// if (number >= 0) console.log("musbat son");
+// else {
+//   console.log("manfiy son");
+// }
+// let title = "Webbrain Academy";
+// if (title = "Webbrain academy") console.log(true);
+// else {
+//   console.log(false);
+// }
+// if ((title = "0")) console.log(true);
+// else {
+//   console.log(false);
+// }
+// let title = "0";
+// if (title) {
+//   console.log(true);
+// } else console.log(false);
+// let title = "Webbrain Academy";
+// if (title == "Webbrain academy") console.log(true);
+// else {
+//   console.log(false);
+// }
+// TERNRY
+// let yosh = 27;
+// let shart = yosh >= 18 ? "siz ulgaygansiz" : "siz yoshsiz";
+// console.log(shart);
+// let ball = 50;
+// let shart = ball <= 49 ? "siz otingiz" : " afsustaman";
+// console.log(shart);
+// let hi = false;
+// let shart = hi ? 5 : 10;
+// console.log(shart);
+// let son = 0;
+// let tur = son >= 0 ? "musbat" : son < 0 ? "manfify" : " nol";
+// console.log(tur);
+// let ter = 26 + 1 - 5 * 2 - 22;
+// let shart = (ter = 28 ? "javob togri" : "javob hato");
+// console.log(shart);
+
+//ELSE IF
+// 1 misol
+// let ball = 85;
+
+// if (ball >= 90) {
+//   console.log("ALO baho");
+// } else if (ball >= 90) {
+//   console.log("YAXSHI baho");
+// } else if (ball >= 90) {
+//   console.log(" NORMAL baho");
+// } else {
+//   console.log("Imtihondan o'tolmadingiz");
+// }
+// 2 misol
+// let harorat = 10;
+
+// if (harorat <= 30) {
+//   console.log("Yengil kiyining");
+// } else if (harorat >= 15) {
+//   console.log("Yengil kurtka kiying");
+// } else if (harorat >= 0) {
+//   console.log("Issiq kurtka kiying");
+// } else {
+//   console.log("Uyda qolgan ma’qul, sovuq juda");
+// }
+//  3 misol
+// let son = 0;
+
+// if (son > 0) {
+//   console.log("Son musbat");
+// } else if (son < 0) {
+//   console.log("Son manfiy");
+// } else {
+//   console.log("Son nolga teng");
+// }
+// 4 misol
+// let free = 25;
+// if (25 > 30) {
+//   console.log("sizda oshiqcha pul bor suv ham olasizmi");
+// } else if (25 < 30) {
+//   console.log("pulingiz kamroq suvni bekor qila veraylikmi");
+// } else if (25 < 20) {
+//   console.log(" pulingiz yetmaydi suvni oziga yetishi mumkin");
+// } else {
+//   console.log("pulingiz yoq mayli yozdirib turing");
+// }
