22e92f6c167d2c2b5a0ea69f7fa6c6b711959b5f
IlyosbekKazimjanov <ilyosbek.351.60.96@gmail.com>
2025-05-19 20:57:37 +0500

    dars

---
+++ 4dars/4-dars.js
@@ -0,0 +1,34 @@
+// if bu agar degani agar buyruq rost bolsa chiqadi
+//else aks holda degani  buyruq yolgon bolsa chiqadi
+// if (shart) { code
+//}  else{ code }
+// let age = 14;
+// if (age >= 18) {
+//   console.log("siz kattasiz");
+// } else {
+//   console.log("sizni yoshingiz kichkina");
+// }
+
+// ternery operatori qisqartma
+// if = ?
+// else = :
+// sintac
+// shart ? chiqadigan javob : notogri bolsa chiqadigan javob
+
+// else if bir nechta shartlar bajarish uchun kerak
+
+// let baho = 5;
+// if (baho == 5) {
+//   console.log("Alo");
+// }
+// else if (baho == 3) {
+//   console.log("yaxshiro oqi ");
+// } 
+// else if (baho == 5) {
+//   console.log("yaxshiro oqi ");
+// } 
+// else if (baho == 3) {
+//   console.log("yaxshiro oqi ");
+// } else {
+//   console.log("O'qi");
+// }
