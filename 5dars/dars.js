d06a657d12e3ee4659e2638bdc51a2b23d18e59d
IlyosbekKazimjanov <ilyosbek.351.60.96@gmail.com>
2025-05-20 07:38:28 +0500

    for while do while

---
+++ 5dars/dars.js
@@ -0,0 +1,108 @@
+// let day = 2;
+// switch (day) {
+//   case 1:
+//     console.log("dushanba ");
+//     break;
+//   case 2:
+//     console.log("seshanba ");
+//     break;
+//   case 3:
+//     console.log("chorshanba ");
+//     break;
+
+//   case 4:
+//     console.log("payshanba ");
+//     break;
+//   case 5:
+//     console.log("juma ");
+//     break;
+// }
+// let day = 2;
+// switch (day) {
+//   case 1:
+//     if (day <= 1) {
+//       console.log("seshanbani");
+//     } else {
+//       (" seshanba emas");
+//     }
+//     break;
+//   case 2:
+//     if (day <= 2) {
+//       console.log("seshanba");
+//     } else {
+//       (" seshanba emas");
+//     }
+//     break;
+//   case 2:
+//     if (day <= 3) {
+//       console.log("seshanbani");
+//     } else {
+//       (" seshanba emas");
+//     }
+//     break;
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log("sonlar", i);
+// }
+// for (let i = 2; i <= 2; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// for (let i = 3; i <= 3; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// for (let i = 4; i <= 4; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// for (let i = 5; i <= 5; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// for (let i = 6; i <= 6; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// for (let i = 7; i <= 7; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// for (let i = 8; i <= 8; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// for (let i = 9; i <= 9; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+// let i = 5;
+// while (i > 0) {
+//   console.log(i);
+//   i--;
+// }
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10000);
+// let yosh;
+// let yoshi = yosh ?? 18;
+// console.log(yoshi);
+// let yosh;
+// let yoshi = yosh ?? !(null || 1);
+// console.log(yoshi);
