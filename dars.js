b99d0a3c68d738d2dbc0c5c28e46c411bd466d9f
IlyosbekKazimjanov <ilyosbek.351.60.96@gmail.com>
2025-05-22 13:23:29 +0500

    dars

---
+++ dars.js
@@ -0,0 +1,118 @@
+// let day = 2;
+// switch (day) {
+//   case 1:
+//     console.log("dushanba ");
+//     break;
+//   case 2:
+//     console.log("seshanba ");
+//     break;
+//   case 3:
+//     console.log("chorshanba ");
+//     break;
+
+//   case 4:
+//     console.log("payshanba ");
+//     break;
+//   case 5:
+//     console.log("juma ");
+//     break;
+// }
+// let day = 2;
+// switch (day) {
+//   case 1:
+//     if (day <= 1) {
+//       console.log("seshanbani");
+//     } else {
+//       (" seshanba emas");
+//     }
+//     break;
+//   case 2:
+//     if (day <= 2) {
+//       console.log("seshanba");
+//     } else {
+//       (" seshanba emas");
+//     }
+//     break;
+//   case 2:
+//     if (day <= 3) {
+//       console.log("seshanbani");
+//     } else {
+//       (" seshanba emas");
+//     }
+//     break;
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log("sonlar", i);
+// }
+// for (let i = 3; i <= 3; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// for (let i = 4; i <= 4; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// for (let i = 5; i <= 5; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// for (let i = 6; i <= 6; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// for (let i = 7; i <= 7; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// for (let i = 8; i <= 8; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// for (let i = 9; i <= 9; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+// let i = 1;
+// let i = 5;
+// while (i > 0) {
+//   console.log(i);
+//   i--;
+// }
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10000);
+// let yosh;
+// let yoshi = yosh ?? 18;
+// console.log(yoshi);
+// let yosh;
+// let yoshi = yosh ?? !(null || 1);
+// console.log(yoshi);
+
+// let x = 5;
+// let j = 1;
+// while (x > 1) {
+//   j *= x;
+//   x--;
+// }
+// console.log(j);
+
+// for (let i = 2; i <= 2; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     console.log(`${i}*${j}=${i * j}`);
+//   }
+// }
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
---
+++ node.js
@@ -0,0 +1,26 @@
+// let a = 2;
+
+// switch (a) {
+//   case value: break;
+
+//   default: break;
+// }
+//switch case bu break chiqmaguncha davom etadi breakchiqsa shart tugaydi
+// for (let i = 1 i<=5; i++ ){
+// console.log(i)
+// }
+// loop takrorlaydi
+// while true qiymat qaytarsa kakrorlayveradi true qiymat tugasa while tugaydi
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+// do while avval ish qilib turib keyn oylaydi
+// let i = 1;
+// do {
+//   console.log("salom!", i);
+//   i++;
+// } while (i <= 100);
+// null undifined null ni false deb oladi qolganlarini true ??
+// console.log("undifined" ?? (null || undefined) ?? "salom");
