323a48828d89f1acbfbdf729a682f7ae8aad61a8
IlyosbekKazimjanov <ilyosbek.351.60.96@gmail.com>
2025-05-22 13:18:00 +0500

    vazifa

---
+++ vazifa.js
@@ -0,0 +1,34 @@
+// — nullish operatori nima?
+// — nullish bilan OR'ning farqi nima ?
+// — nullish bilan AND ning farqi nima?
+// — ? vs ?? ning farqi nimada?
+// javob
+// nullish bu  undifined null ni false deb oladi qolganlarini true
+//nullish bu null va undifined ni false deb oladi or esa false qiymatlilarni bari false ga teng
+//AND 0  ni false deb oladi nullish esa 0 ni true deb olaedi
+// ? bu end ?? bu nullish
+
+// — Switch bilan if ning farqi nimada?
+// — Switch bilan ternary operatorning farqi nimada?
+//javob
+// switch da conditionni orniga varieble   yoziladi  if da condition va switch taqoslash operatorlarini faqat === likini oladi
+// terneryda qisqartma shaklda condition yoziladi
+
+// — For loop nima maqsadda ishlatiladi?
+// — for loopda parametori nechta va nima vazifa bajaradi?
+// — For loop parameterlarining tushurib qoldirilishi holatlari haqida malumot bering.
+// — for loopda var bilan let farqi
+// — faqatgina ichki for loopni to'htatish uchun nima qilinadi.
+//javob
+// for loop da ham qaytaradi takror qilishi
+// for (let i = 1 i<=5; i++ ){console.log(i)}
+// oxiridagi i ++ bita aylani kelib keyin ishladi
+// var global let local
+// ichki  for ga break deb yozamiz
+
+// let i = " "
+// for(let j = 0; j<=5 ; j++){
+//    i += " *"
+//     console.log(i);
+    
+// }
